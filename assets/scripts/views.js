08566ae940e341a5ea3d4e398e5a9e3e6a482b77
noobiwankenoobi <ross.hazelett@gmail.com>
2017-06-14 11:55:39 -0400

    Views and Nav
    I added a basic bootstrap nav bar with a link to lists,
    and auth functions
    I added a view.js file to handle all handlebars template switching
    Similar to layout.js file used in a previous project

---
+++ assets/scripts/views.js
@@ -0,0 +1,12 @@
+const store = require('./store.js')
+
+const loadGamesLists = (response) => {
+  const gamesListTemplate = require('./templates/gamesListView.handlebars')
+  const content = gamesListTemplate({ games: response.games })
+  $('#app').empty()
+  $('#app').html(content)
+}
+
+module.exports = {
+  loadGamesLists
+}
